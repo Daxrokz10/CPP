ba4671aba50ff193d8958de380588f6f2e58a784
Daxrokz10 <dakshgagnani@gmail.com>
2025-01-30 12:43:50 +0530

    sum of first and last

--- practice.cpp
+++ practice.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
-int main(){
-   const float PI = 3.14;
 
-   cout<<PI;
+int main(){
+   int num;
+
+   cout<<"Enter your number: ";
+   cin >> num;
+   int firstdig = num % 10;
+   int lastdig = 0;
+   while(num>=10){
+      num /= 10;
+      if(num<=10){
+         lastdig = num;
+      }
+   }
+   int sum = firstdig + lastdig;
+   cout<<"Sum of first and last digits is : "<<sum;
 }
