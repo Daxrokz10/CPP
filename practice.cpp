d4d7dd9bec4ebbe20781ab78dc216f08eb07209e
Daxrokz10 <dakshgagnani@gmail.com>
2025-01-30 12:18:23 +0530

    test

--- practice.cpp
+++ practice.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int i = 0;
+   const float PI = 3.14;
 
-    for(i=1;i<=5;i++){
-        for(int j=i;j>=1;j--){
-            cout<<"* ";
-        }
-        cout<<endl;
-    }
-    return 0;
+   cout<<PI;
 }
