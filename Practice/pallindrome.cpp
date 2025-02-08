f9eda356e3bbc541b4711d8a18bc3dad08325db4
Daxrokz10 <dakshgagnani@gmail.com>
2025-02-08 11:00:50 +0530

    practice

---
+++ Practice/pallindrome.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+
+    cout<<"Enter number";
+    cin>>num;
+
+    int temp = num;
+    int reversedNum = 0;
+
+    while(temp != 0){
+        int digit = temp % 10;
+        reversedNum = reversedNum * 10 + digit;
+        temp /= 10;
+    }
+
+    if(reversedNum == num){
+        cout<<num<<" is a pallindrome number!"<<endl;
+    }else{
+        cout<<num<<" is not a pallindrome number!"<<endl;
+    }
+    
+}
