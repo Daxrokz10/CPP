0d24e7ac5e9bcf78f9cc3f464040f7da8093372e
Daxrokz10 <dakshgagnani@gmail.com>
2025-02-19 11:02:04 +0530

    patterns

--- Labwork/Patterns/p8.cpp
+++ Labwork/Patterns/p8.cpp
@@ -1,24 +1,16 @@
-//    *
-//   * *
-//  * * *
 // * * * *
 //  * * *
 //   * *
 //    *  
+//    *
+//   * *
+//  * * *
+// * * * *
 
 #include<iostream>
 using namespace std;
 
 int main(){
-    for(int i=1;i<=3;i++){
-        for(int s=4;s>=i;s--){
-            cout<<" ";
-        }
-        for(int j=1;j<=i;j++){
-            cout<<"* ";
-        }
-        cout<<endl;
-    }
     for(int i=4;i>=1;i--){
         for(int s=4;s>=i;s--){
             cout<<" ";
@@ -28,4 +20,14 @@ int main(){
         }
         cout<<endl;
     }
+    for(int i=1;i<=4;i++){
+        for(int s=4;s>=i;s--){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+ 
 }
