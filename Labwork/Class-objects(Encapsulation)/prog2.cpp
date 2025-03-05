7f42dd359ba3106a23869656263b4ebdaeee3e76
Daxrokz10 <dakshgagnani@gmail.com>
2025-03-05 12:36:20 +0530

    dynamic vector object

---
+++ Labwork/Class-objects(Encapsulation)/prog2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Student {
+    private:
+        int rollno;
+        string name;
+
+    public:
+        void setStdDetails(int i) {
+            cout << "Enter the roll no for student " << i << " : ";
+            cin >> rollno;
+            cout << "Enter the name for student " << i << " : ";
+            cin >> name;
+        }
+
+        void getStdDetails(int i) {
+            cout << "Roll no for student " << i << " : " << rollno << endl;
+            cout << "Name for student " << i << " : " << name << endl;
+        }
+};
+
+int main() {
+    int size;
+    cout << "Enter the number of students: ";
+    cin >> size;
+
+    vector<Student> students(size);
+
+    for (int i = 0; i < size; i++) {
+        students[i].setStdDetails(i);
+    }
+
+    for (int i = 0; i < size; i++) {
+        students[i].getStdDetails(i);
+    }
+
+    return 0;
+}
