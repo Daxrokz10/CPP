62d1ef0ee5f8216a48442d65986e215f1596872e
Daxrokz10 <dakshgagnani@gmail.com>
2025-03-26 13:46:55 +0530

    labwork done

---
+++ Labwork/CRUD/vector_crud.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class VectorCrud{
+    public:
+        vector<int> v1;
+
+    void viewElement(){
+        for(int i=0;i<v1.size();i++){
+            cout<<"v1["<<i<<"] : "<<v1[i]<<endl;
+        }
+    }
+    void insertElement(int element){
+        v1.push_back(element);
+    }
+    void updateElement(int index,int element){
+        if(index >= 0 && index < v1.size()){
+            v1[index] = element;
+        }else{
+            cout<<"invalid index";
+        }
+    }
+    void deleteElement(int index){
+        v1.erase(v1.begin() + index);
+    }
+};
+
+int main()
+{
+    VectorCrud vec;
+    int element,index,choice;
+
+    do{
+
+        cout << "=============================" << endl;
+        cout << "         CRUD MENU           " << endl;
+        cout << "=============================" << endl;
+        cout << "1. View Elements" << endl;
+        cout << "2. Insert Element" << endl;
+        cout << "3. Update Element" << endl;
+        cout << "4. Delete Element" << endl;
+        cout << "0. Exit" << endl;
+        cout << "=============================" << endl;
+        cout << "Enter your choice: ";
+        cin>>choice;
+
+        switch(choice){
+            case 1:
+                vec.viewElement();
+                break;
+            case 2:
+                cout<<"Enter element: ";
+                cin>>element;
+                vec.insertElement(element);
+                break;
+            case 3:
+                cout<<"Enter index of element to update: ";
+                cin>>index;
+                cout<<"Enter new element: ";
+                cin>>element;
+                vec.updateElement(index,element);
+                break;
+            case 4:
+                cout<<"Enter index of element to delete: ";
+                cin>>index;
+                vec.deleteElement(index);
+                break;
+            case 0:
+                break;
+            default:
+                cout<<"Invalid choice"<<endl;
+                break;
+        }
+    }while(choice != 0);
+
+    return 0;
+}
