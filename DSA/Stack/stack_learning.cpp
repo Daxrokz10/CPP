3235e2f1e71b8f9b0295eb88302e71181fbd5489
Daxrokz10 <dakshgagnani@gmail.com>
2025-04-11 22:51:00 +0530

    stack

---
+++ DSA/Stack/stack_learning.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+int stack[100] , top = -1;
+
+void push(int data){
+    top++;
+    stack[top] = data;
+}
+
+void pop(){
+    stack[top] = NULL;
+    top--;
+}
+
+void view(){
+    cout<<"The elements of the stack are: ";
+    for(int i=0;i<=top;i++){
+        cout<<"Stack["<<i<<"] :"<<stack[i];
+        cout<<endl;
+    }
+}
+int main()
+{   
+    int n;
+    cout << "Enter the number of elements to be pushed in stack: ";
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int data;
+        cout << "Enter the element to be pushed: ";
+        cin >> data;
+        push(data);
+    }
+    cout << "The elements in the stack are: ";
+    for(int i = 0; i <= top; i++){
+        cout << stack[i] << " ";
+    }
+
+    pop();
+    cout << "\nThe elements in the stack after popping are: ";
+    for(int i = 0; i <= top; i++){
+        cout << stack[i] << " ";
+    }
+    cout << "\nThe top element in the stack is: " << stack[top] << endl;
+
+    view();
+    
+    return 0;
+}
---
+++ DSA/Stack/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+cout<<"Push item into stack: ";
+    cin>>n;
+    push(n);
+    cout<<"Top element of stack: "<<stack[top]<<endl;
